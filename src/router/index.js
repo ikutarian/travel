9ed16141f49906ab1a6635d3a9257196cb4963a4
ikutarian <saitou0217@outlook.com>
2020-08-13 21:16:13 +0800

    - 添加两个组件：Home和Vue - 配置@/router/index.js，添加两个路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '@/pages/home/Home'  // @是src目录的别名
+import List from '@/pages/list/List'
 
 Vue.use(VueRouter)
 
-  const routes = [
-  {
+export default new VueRouter({
+  routes: [{
     path: '/',
-    name: 'Home',
     component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
-const router = new VueRouter({
-  routes
+  }, {
+    path: '/list',
+    component: List
+  }]
 })
-
-export default router
