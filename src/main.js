4b4f1b45784726ff5308332eac346d8f1b6e57d6
ikutarian <saitou0217@outlook.com>
2020-08-14 22:17:57 +0800

    - 实现HomeHeader组件 - 引入iconfont

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import './assets/styles/reset.css' // 重置样式表，使得在不同设备上有一个一致的表现
 import './assets/styles/border.css' // 解决不同设备上1px边框不一致的问题
 import fastClick from 'fastclick' // 解决移动端300ms点击延迟的问题
+import './assets/styles/iconfont.css' // 引入iconfont
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
